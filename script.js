16bd3632a583b6ed932208ad647d7cc0957f3313
Darshan Pagar <mrdarshanpagar@gmail.com>
2022-04-30 16:31:45 +0530

    created Tile class and modified script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-import Grid from './Grid.js'
+import Grid from "./Grid.js";
+import Tile from "./Tile.js";
 
-const gameBoard = document.getElementById('game-board')
-const grid = new Grid(gameBoard)
-
+const gameBoard = document.getElementById("game-board");
+const grid = new Grid(gameBoard);
+console.log(grid.randomEmptyCell());
+grid.randomEmptyCell().tile = new Tile(gameBoard);
+grid.randomEmptyCell().tile = new Tile(gameBoard);
