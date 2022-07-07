f97a086940c31b5380a3a67371b552c13739d328
Darshan Pagar <mrdarshanpagar@gmail.com>
2022-07-07 18:46:34 +0530

    added logic to handle inputs

--- Grid.js
+++ Grid.js
@@ -1,61 +1,134 @@
-const GRID_SIZE = 4
-const CELL_SIZE = 20
-const CELL_GAP = 2
+const GRID_SIZE = 4;
+const CELL_SIZE = 20;
+const CELL_GAP = 2;
 
 export default class Grid {
-    #cells
-    constructor(gridElement){
-        gridElement.style.setProperty('--grid-size',GRID_SIZE)
-        gridElement.style.setProperty('--cell-size',`${CELL_SIZE}vmin`)
-        gridElement.style.setProperty('--cell-gap',`${CELL_GAP}vmin`)
-        this.#cells = createCellElements(gridElement).map((cellElement, index) => {
-            return new Cell(cellElement, index % GRID_SIZE, Math.floor(index / GRID_SIZE))
-        })
-        console.log(this.#cells)
-    }
+  #cells;
 
-    get #emptyCells(){
-        return this.#cells.filter(cell => cell.tile == null)
-    }
+  constructor(gridElement) {
+    gridElement.style.setProperty("--grid-size", GRID_SIZE);
+    gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
+    gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
+    this.#cells = createCellElements(gridElement).map((cellElement, index) => {
+      return new Cell(
+        cellElement,
+        index % GRID_SIZE,
+        Math.floor(index / GRID_SIZE)
+      );
+    });
+  }
 
-    randomEmptyCell(){
-        const randomIndex = Math.floor(Math.random() * this.#emptyCells.length)
-        return this.#emptyCells[randomIndex]
-    }
+  get cells() {
+    return this.#cells;
+  }
+
+  get cellsByRow() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.y] = cellGrid[cell.y] || [];
+      cellGrid[cell.y][cell.x] = cell;
+      return cellGrid;
+    }, []);
+  }
+
+  /* arrange cells by columns
+  // turn rows into columns and columns into rows, so that
+  // cellGrid[0] represent 1st column of game-board grid
+  // and cellGrid[1] represent 2nd column of game-board grid
+     insert all column elments in single array(row)
+  */
+
+  get cellsByColumn() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.x] = cellGrid[cell.x] || [];
+      cellGrid[cell.x][cell.y] = cell;
+      return cellGrid;
+    }, []);
+  }
+
+  get #emptyCells() {
+    return this.#cells.filter((cell) => cell.tile == null);
+  }
+
+  randomEmptyCell() {
+    const randomIndex = Math.floor(Math.random() * this.#emptyCells.length);
+    return this.#emptyCells[randomIndex];
+  }
 }
 
 class Cell {
-    #cellElement
-    #x
-    #y
-    #tile
+  #cellElement;
+  #x;
+  #y;
+  #tile;
+  #mergeTile;
 
-    constructor(cellElement, x, y){
-        this.#cellElement = cellElement
-        this.#x = x
-        this.#y = y
-    }
+  constructor(cellElement, x, y) {
+    this.#cellElement = cellElement;
+    this.#x = x;
+    this.#y = y;
+  }
 
-    get tile(){
-        return this.#tile;
-    }
+  get x() {
+    return this.#x;
+  }
 
-    set tile(value){
-        this.#tile = value
-        if(value == null) return
-        this.#tile.x = this.#x
-        this.#tile.y = this.#y
-    }
+  get y() {
+    return this.#y;
+  }
+
+  get tile() {
+    return this.#tile;
+  }
+
+  set tile(value) {
+    this.#tile = value;
+    if (value == null) return;
+    this.#tile.x = this.#x;
+    this.#tile.y = this.#y;
+  }
+
+  get mergeTile() {
+    return this.#mergeTile;
+  }
+
+  set mergeTile(value) {
+    this.#mergeTile = value;
+    if (value == null) return;
+    this.#mergeTile.x = this.#x;
+    this.#mergeTile.y = this.#y;
+  }
+
+  /*
+    for moveUp, if above tile in empty(null) we can move up
+    if above tile value is equal to below tile value, then we can move up
+    if above tile is result of already merged tile, and its value is equal to below tile
+       then we cannot merge.Because we can perform only one merge operation at a time.
+       else {
+        merge if this.merge(above tile) = null i.e.(not result of merged tile)
+       }
+  */
+  canAccept(tile) {
+    return (
+      this.tile == null ||
+      (this.mergeTile == null && this.tile.value === tile.value)
+    );
+  }
+
+  mergeTiles() {
+    if (this.tile == null || this.mergeTile == null) return;
+    this.tile.value = this.tile.value + this.mergeTile.value;
+    this.mergeTile.remove();
+    this.mergeTile = null;
+  }
 }
 
-function createCellElements(gridElement){
-    const cells = []
-    for(let i = 0; i < GRID_SIZE * GRID_SIZE; i++){
-        const cell = document.createElement('div')
-        cell.classList.add('cell')
-        cells.push(cell)
-        gridElement.append(cell)
-    }
-
-    return cells
+function createCellElements(gridElement) {
+  const cells = [];
+  for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cells.push(cell);
+    gridElement.append(cell);
+  }
+  return cells;
 }
--- Tile.js
+++ Tile.js
@@ -36,4 +36,18 @@ export default class Tile {
     this.#y = value;
     this.#tileElement.style.setProperty("--y", value);
   }
+
+  get value(){
+    return this.#value
+  }
+
+  remove(){
+    this.#tileElement.remove()
+  }
+
+  waitForTransition(){
+    return new Promise(resolve=>{
+      this.#tileElement.addEventListener('transitionend',resolve,{once:true})
+    })
+  }
 }
